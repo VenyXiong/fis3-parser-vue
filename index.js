88fbc2b40934d2e6118e87177066a30a00f09fac
xiongwenjie <xiongwenjie@baidu.com>
2016-06-28 21:24:02 +0800

    fixed html parse bug

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function (content, file, conf) {
   var result = '';
   // 用 HTML 相关语言能力处理一遍
   if (fis.compile.partial && file.ext === '.vue') {
-    content = fis.compile.partial(content, file, {
+    content = fis.compile.partial(content.toString('utf8'), file, {
       ext: '.html',
       isHtmlLike: true
     });
